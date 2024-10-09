d127c8c08c7abbce60ccef2f9c5e05a0798f2272
telemakos <dbcahilly3@gmail.com>
2024-10-08 22:04:05 -0500

    bruh

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,11 @@ fn main() -> ! {
         led_tx.set_high();
         led_rx.set_low();
         delay_ms(1000);
-        // ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").unwrap_infallible();
-        serial.write_str("test").unwrap_infallible();
-        serial.flush();
-        // // Read a byte from the serial connection
-        _ = nb::block!(serial.read()).unwrap_infallible();
+        ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").unwrap_infallible();
+        // serial.write_str("test").unwrap_infallible();
+        // serial.flush();
+        // // // Read a byte from the serial connection
+        // _ = nb::block!(serial.read()).unwrap_infallible();
 
         // // Answer
         // ufmt::uwriteln!(&mut serial, "Got {}!\r", b).unwrap_infallible();
